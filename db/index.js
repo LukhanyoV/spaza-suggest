822c3dc67e8b000c29c6a6753adc6ea68db81eee
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-11-23 16:54:59 +0200

    heroku database url

--- db/index.js
+++ db/index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 const pgp = require("pg-promise")({});
 const isProduction = process.env.NODE_ENV === "production";
-
-const databaseURL = isProduction ? process.env.MY_DATABASE_URL : process.env.LOCAL_DATABASE_URL;
+const databaseURL = process.env.LOCAL_DATABASE_URL;
 
 const config = {
     connectionString: databaseURL
 };
 
 if(isProduction){
+    config.connectionString = process.env.DATABASE_URL;
     config.ssl = {
         rejectUnauthorized: false
     }
